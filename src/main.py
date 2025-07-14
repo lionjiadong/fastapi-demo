1c389d264af774e1378d7e1a83a96a8de528e2a6
梁嘉栋 <liangjiadong@LKMac-mini.local>
2025-07-14 18:10:21 +0800

    tt

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+import time
 from functools import lru_cache
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.middlewares import add_process_time_header
 
 from .api import api_router
 from .config import settings
 
-print(settings)
 app = FastAPI()
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
