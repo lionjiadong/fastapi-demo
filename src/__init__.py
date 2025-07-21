d2ada9b4413510280430e0c4d682f12461bf1c82
梁嘉栋 <liangjiadong@LKMac-mini.local>
2025-07-21 19:01:57 +0800

    tttt

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,3 @@
-# from typing import TYPE_CHECKING
-
-# if TYPE_CHECKING:
-
-
-# if TYPE_CHECKING:
-from src.test.link import HeroTeamLink
-from src.test.team import Team
-from src.test.hero import Hero
-
 from src.user.models.links import UserRoleLink
 from src.user.models.user import User
 from src.user.models.role import Role
--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
-import time
-from functools import lru_cache
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic import ValidationError
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
 
 from src.exceptions import validation_exception_handler
 from src.middlewares import add_process_time_header
 
 from .api import api_router
 from .config import settings
+
 
 app = FastAPI(
     middleware=[
@@ -23,5 +28,36 @@ app = FastAPI(
     exception_handlers={
         ValidationError: validation_exception_handler,
     },
+    docs_url=None,
+    redoc_url=None,
+    openapi_url="/openapi.json",
+    swagger_ui_oauth2_redirect_url="/docs/oauth2-redirect",
 )
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(api_router)
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+        swagger_favicon_url="/static/image.png",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
